f88f799395edecb5019a798415aa5bf6483c27ac
kepcher2131 <pashinka2002@gmail.com>
2023-05-21 02:09:36 +0400

    _

---
+++ LABA_1/laba_1/Controllers/HomeController.cs
@@ -0,0 +1,94 @@
+﻿using laba_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace laba_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Calc cal, string name)
+        {
+            byte a = Convert.ToByte(cal.operand1);
+            byte b = Convert.ToByte(cal.operand2);
+            float result = Convert.ToSingle(cal.result);
+
+            switch (cal.calulate)
+            {
+                case "-":
+                    result = a - b;
+                    cal.exception = "";
+                    break;
+                case "+":
+                    result = a + b;
+                    cal.exception = "";
+                    break;
+                case "*":
+                    result = a * b;
+                    cal.exception = "";
+                    break;
+                case "/":
+                    if (b == 0)
+                    {
+                        cal.exception = "На ноль делить нельзя";
+                    }
+                    else
+                    {
+                        result = a / b;
+                        cal.exception = "";
+                    }
+                    break;
+            }
+
+            if (name == "delete")
+            {
+                cal.operand1 = "";
+                cal.operand2 = "";
+                cal.result = "";
+                cal.exception = "";
+            }
+
+            ViewData["result"] = result;
+            ViewData["exception"] = cal.exception;
+
+            if (ViewBag.result == 5)
+            {
+                ViewBag.exception = "Вычисленный результат равен заданному значению";
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public string Duplication(string operand1, string calulate, string operand2, string result)
+        {
+            string Operand1 = Request.Query["operand1"];
+            string Calulate = Request.Query["calulate"];
+            string Operand2 = Request.Query["operand2"];
+            string Result = Request.Query["result"];
+            return $"{operand1} {calulate} {operand2} {result}";
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LABA_1/laba_1/Models/Calc.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace laba_1.Models
+{
+    public class Calc
+    {
+        [Required(ErrorMessage = "Введите число")]
+        public string? operand1 { get; set; }
+        [Required(ErrorMessage = "Введите число")]
+        [StringLength(3, MinimumLength = 2, ErrorMessage = "Длина символа должна быть от 2 до 3")]
+        public string? operand2 { get; set; }
+        public string result { get; set; }
+        public string calulate { get; set; }
+        public string exception { get; set; }
+    }
+}
