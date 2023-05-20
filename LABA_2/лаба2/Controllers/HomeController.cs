dc549b846b68098307ce70618f18569df1e57e80
kepcher2131 <pashinka2002@gmail.com>
2023-05-21 03:01:11 +0400

    _

---
+++ LABA_2/лаба2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+using laba_2.Models;
+
+namespace laba_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string id, string Fullname, DateTime data, uint number, string adress_sender, string adress_recipient, string weight, string delivery_method)
+        {
+            string authData = $"ID: {id}  Name: {Fullname}  Delivery_date: {data}  Phone: {number}  Adress_sender: {adress_sender}  Adress_recipient: {adress_recipient}  Weight: {weight}  Method: {Convert.ToString(delivery_method)}";
+            return Content(authData);
+        }
+        public IActionResult details()
+        {
+            return View(User);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LABA_2/лаба2/Models/dataset.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace laba_2.Models
+{
+    [DisplayName("Информация о заказе")]
+    public class dataset
+    {
+        public string? id { get; set; }
+
+        [DisplayName("ФИО")]
+        public string? name { get; set; }
+
+        [DisplayName("Дата")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? data { get; set; }
+
+        [DisplayName("Номер телефона")]
+        [DataType(DataType.PhoneNumber)]
+        public uint number { get; set; }
+
+        [DisplayName("Текущее место")]
+        [DataType(DataType.MultilineText)]
+        public string? adress_sender { get; set; }
+
+        [DisplayName("Место выдачи")]
+        [DataType(DataType.MultilineText)]
+        public string? adress_recipient { get; set; }
+
+        [DisplayName("Вес посылки")]
+        public string? weight { get; set; }
+        
+        [DisplayName("Способ доставки")]
+        public bool delivery_method { get; set; }
+
+    }
+
+    public enum delivery_method
+    {
+        regular,
+        expedited,
+        by_courier
+
+    }
+}
