e06108dd832906c4e9e5458d1175a99a456860f6
kepcher2131 <pashinka2002@gmail.com>
2023-05-21 03:51:29 +0400

    _

---
+++ LABA_3/WebApplication8/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Mvc;
+using Filters.Infrastructure;
+
+
+public class HomeController : Controller
+{
+    //[CustomAuth(false)]
+    //[Authorize(Users = null, Roles = "admin")]
+    public ActionResult Index()
+    {
+        return View();
+
+    }
+   
+    [HttpPost]
+    public ActionResult Index(string name, string lastname, string type, int? elevation, string adress, bool open)
+    {
+        if (elevation.HasValue)
+        {
+            ViewBag.Name = name;
+            ViewBag.LastName = lastname;
+            ViewBag.Type = type;
+            ViewBag.Elevation = elevation.Value;
+            ViewBag.Adress = adress;
+            ViewBag.Open = open;
+            return View("Result");
+        }
+        else
+        {
+            return RedirectToAction("Index");
+        }
+    }
+
+    [HandleError(ExceptionType = typeof(ArgumentOutOfRangeException), View = "RangeError")]
+    public string RangeTest(int id)
+    {
+        if (id > 100)
+        {
+            return String.Format("The id value is: {0}", id);
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+
+
+}
---
+++ LABA_3/WebApplication8/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("WebApplication8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("WebApplication8")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения false в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM. Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("dc0f0851-ca7c-4c2e-96a2-e3d377cd8735")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      дополнительный номер версии;
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
